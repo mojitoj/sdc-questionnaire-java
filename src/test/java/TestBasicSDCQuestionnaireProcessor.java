e2098cc90a2ebee989bd47c6115d87c691ff4d6f
Mohammad Jafari <jafarim@gmail.com>
2022-02-26 19:25:44 -0800

    adding test for count function similar to acorn questionnaire

--- src/test/java/TestBasicSDCQuestionnaireProcessor.java
+++ src/test/java/TestBasicSDCQuestionnaireProcessor.java
@@ -11,9 +11,19 @@ import java.nio.file.Paths;
 
 public class TestBasicSDCQuestionnaireProcessor {
     @Test
-    public void transformTest() throws IOException, FHIRParserException, FHIRPathException, FHIRPatchException, FHIRGeneratorException {
+    public void transformTest1() throws IOException, FHIRParserException, FHIRPathException, FHIRPatchException, FHIRGeneratorException {
         String questionnaireFilePath = "src/test/fixtures/sdc-questionnaire-sample.json";
         String questionnaireResponseFilePath = "src/test/fixtures/sdc-questionnaire-response-sample.json";
+        String questionnaireString = new String(Files.readAllBytes(Paths.get(questionnaireFilePath)));
+        String questionnaireResponseString = new String(Files.readAllBytes(Paths.get(questionnaireResponseFilePath)));
+        String transformedQuestionnaireResponse = BasicSDCQuestionnaireProcessor.transform(questionnaireString, questionnaireResponseString);
+        System.out.println(transformedQuestionnaireResponse);
+    }
+
+    @Test
+    public void transformTest2() throws IOException, FHIRParserException, FHIRPathException, FHIRPatchException, FHIRGeneratorException {
+        String questionnaireFilePath = "src/test/fixtures/acorn-questionnaire-sample.json";
+        String questionnaireResponseFilePath = "src/test/fixtures/acorn-questionnaire-response-sample.json";
         String questionnaireString = new String(Files.readAllBytes(Paths.get(questionnaireFilePath)));
         String questionnaireResponseString = new String(Files.readAllBytes(Paths.get(questionnaireResponseFilePath)));
         String transformedQuestionnaireResponse = BasicSDCQuestionnaireProcessor.transform(questionnaireString, questionnaireResponseString);
